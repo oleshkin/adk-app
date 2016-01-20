b4594cdf821eb5ed59979e965fe0d4b3990745e7
Aleksandr Oleshko <oleshkin@gmail.com>
2016-01-20 17:39:50 +0200

    Added icons for filetypes and ng-repeat iteration through each file for each freight

--- js/app.js
+++ js/app.js
@@ -16,7 +16,17 @@
 		amount: 2,
 		weight: 256,
 		longstay: false,
-		gone: false
+		gone: false,
+		file: [
+			{
+				filetype: 'pdf',
+				filename: '11911.pdf'
+			}, 
+			{
+				filetype: 'pdf',
+				filename: '11912.pdf'
+			}
+			]
 	},
 	{
 		client: 'Sanqch',
@@ -27,7 +37,17 @@
 		amount: 2,
 		weight: 179.5,
 		longstay: false,
-		gone: false
+		gone: false,
+		file: [
+			{
+				filetype: 'jpg',
+				filename: '123.jpg'
+			}, 
+			{
+				filetype: 'txt',
+				filename: '124.txt'
+			}
+			]
 	},
 	{
 		client: 'Sanqch',
@@ -38,7 +58,17 @@
 		amount: 2,
 		weight: 2000,
 		longstay: false,
-		gone: false
+		gone: false,
+		file: [
+			{
+				filetype: 'doc',
+				filename: '11911.doc'
+			}, 
+			{
+				filetype: 'rar',
+				filename: '11912.rar'
+			}
+			]
 	}
 	];
 
